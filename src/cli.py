fe452fd667f22b5f3c768b3977a9d842c12b8eff
MANISH-BEESA <manishbeesa05@gmail.com>
2026-01-10 18:51:40 +0530

    setup completed

--- src/cli.py
+++ src/cli.py
@@ -1,58 +1,58 @@
-# import argparse
-# from src import data
+import argparse
+from src import data
 
 
-# def init(args):
-#     data.init(args.path)
+def init(args):
+    data.init(args.path)
 
 
-# def hash_objects(args):
-#     data.hash_objects(args.file)
+def hash_objects(args):
+    data.hash_objects(args.file)
 
 
-# def decompress(args):
-#     data.decompress(args.hash)
+def decompress(args):
+    data.decompress(args.hash)
 
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(prog="ding")
+def parse_args():
+    parser = argparse.ArgumentParser(prog="ding")
 
-#     # COMMANDS
-#     commands = parser.add_subparsers(dest="command", required=True)
+    # COMMANDS
+    commands = parser.add_subparsers(dest="command", required=True)
 
-#     ## init
-#     init_parser = commands.add_parser(
-#         "init", help="initializes an empty ding repository"
-#     )
-#     init_parser.add_argument(
-#         "path",
-#         nargs="?",
-#         default=".",
-#         help="directory to initialize the ding repository in (default: current directory)",
-#     )
-#     init_parser.set_defaults(func=init)
+    ## init
+    init_parser = commands.add_parser(
+        "init", help="initializes an empty ding repository"
+    )
+    init_parser.add_argument(
+        "path",
+        nargs="?",
+        default=".",
+        help="directory to initialize the ding repository in (default: current directory)",
+    )
+    init_parser.set_defaults(func=init)
 
-#     ## hash
-#     hash_parser = commands.add_parser("hash", help="hashes and stores the file")
-#     hash_parser.add_argument("file", help="the file to be hashed")
-#     hash_parser.set_defaults(func=hash_objects)
+    ## hash
+    hash_parser = commands.add_parser("hash", help="hashes and stores the file")
+    hash_parser.add_argument("file", help="the file to be hashed")
+    hash_parser.set_defaults(func=hash_objects)
 
-#     ## cat-file
-#     hash_parser = commands.add_parser(
-#         "cat-file", help="prints out the uncompressed data"
-#     )
-#     hash_parser.add_argument(
-#         "hash", help="the hash (or start of hash) of file to decompress"
-#     )
-#     hash_parser.set_defaults(func=decompress)
+    ## cat-file
+    hash_parser = commands.add_parser(
+        "cat-file", help="prints out the uncompressed data"
+    )
+    hash_parser.add_argument(
+        "hash", help="the hash (or start of hash) of file to decompress"
+    )
+    hash_parser.set_defaults(func=decompress)
 
-#     return parser.parse_args()
+    return parser.parse_args()
 
 
 def main(): 
     print("Ding Dong, who's there \nThe IRS \nOh Shit")
-    # args = parse_args()
-    # args.func(args)
+    args = parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
